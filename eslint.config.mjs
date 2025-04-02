7af3e8c5bdcfc12f3b6644a0280b5eb327de83c6
siparat <akimov1712@yandex.ru>
2025-04-02 17:42:11 +0300

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,63 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      ecmaVersion: 5,
+      sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          ignoreRestSiblings: true,
+          caughtErrors: 'none',
+        },
+      ],
+      'prettier/prettier': [
+        'warn',
+        {
+          useTabs: true,
+          indentSize: 4,
+          singleQuote: true,
+          trailingComma: 'none',
+          printWidth: 120,
+          semi: true,
+          bracketSpacing: true,
+          bracketSameLine: true,
+          arrowParens: 'always',
+          parser: 'typescript',
+          endOfLine: 'lf',
+        },
+      ],
+    },
+  },
+);
