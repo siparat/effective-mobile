39955dd9aba41851945ff05035213be6783eaf5e
siparat <akimov1712@yandex.ru>
2025-04-05 13:06:17 +0300

    deleted unused providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
@@ -20,8 +18,6 @@ import { join } from 'path';
 		DatabaseModule,
 		AppealModule,
 		FileModule
-	],
-	controllers: [AppController],
-	providers: [AppService]
+	]
 })
 export class AppModule {}
